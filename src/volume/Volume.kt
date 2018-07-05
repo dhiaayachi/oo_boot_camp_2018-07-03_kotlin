4ed3ea36437d27f99f1f0b60f986922d5f6895e9
Dhia Ayachi <dhia.ayachi@bell.ca>
2018-07-05 13:29:00 -0400

    Volume take 2

--- src/volume/Volume.kt
+++ src/volume/Volume.kt
@@ -2,13 +2,19 @@ package volume
 
 class Volume(n: Number, units: Units) {
     private val n: Double
-    enum class Units {
-        TABLE_SPOON, TEA_SPOON
+    enum class Units(val u: Int) {
+        TEA_SPOON(1),
+        TABLE_SPOON(3 * TEA_SPOON.u),
+        OUNCE(2 * TABLE_SPOON.u),
+        CUP(8 * OUNCE.u),
+        PINT(2 * CUP.u),
+        QUART(2 * PINT.u),
+        GALLON(4 * QUART.u)
     }
 
     init {
-        if (units == Units.TABLE_SPOON) this.n = n.toDouble() * 3.0
-        else this.n = n.toDouble()
+        if (n.toDouble() < 0) throw IllegalArgumentException("Volume must be >= 0")
+        this.n = n.toDouble() * units.u
     }
 
     override fun equals(other: Any?): Boolean {
@@ -18,4 +24,10 @@ class Volume(n: Number, units: Units) {
     private fun equals(other: Volume) = this.n == other.n
 
     override fun hashCode()= this.n.hashCode()
+    override fun toString() = this.n.toString()
+
+    operator fun times(i: Number) = Volume(this.n * i.toDouble(), Units.TEA_SPOON)
+    operator fun plus(other: Volume) = Volume(this.n + other.n,Volume.Units.TEA_SPOON)
+    operator fun minus(other: Volume) = Volume(this.n - other.n, Volume.Units.TEA_SPOON)
+    operator fun div(i: Number) = Volume(this.n/i.toDouble(), Units.TEA_SPOON)
 }
--- test/volume/VolumeTest.kt
+++ test/volume/VolumeTest.kt
@@ -2,15 +2,52 @@ package volume
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
 
 internal class VolumeTest {
     private val tablespoon = Volume(1, Volume.Units.TABLE_SPOON)
-    private val three_teaspoon = Volume(3, Volume.Units.TEA_SPOON)
-    private val teaspoon = Volume(3, Volume.Units.TEA_SPOON)
+    private val teaspoon = Volume(1, Volume.Units.TEA_SPOON)
+    private val ounce =  Volume(1, Volume.Units.OUNCE)
+    private val cup = Volume(1, Volume.Units.CUP)
+    private val pint = Volume(1, Volume.Units.PINT)
+    private val quart = Volume(1, Volume.Units.QUART)
+    private val gallon = Volume(1, Volume.Units.GALLON)
+
+    private val emptyVolume = Volume(0, Volume.Units.GALLON)
 
     @Test fun equals(){
         assertEquals(tablespoon, Volume(1, Volume.Units.TABLE_SPOON))
+        assertEquals(gallon * 1.5, Volume(1.5, Volume.Units.GALLON))
+        assertEquals(emptyVolume, Volume(0, Volume.Units.TABLE_SPOON))
         assertEquals(tablespoon.hashCode(), Volume(1, Volume.Units.TABLE_SPOON).hashCode())
-        assertEquals(tablespoon, three_teaspoon)
+    }
+    @Test fun invalid(){
+        assertFailsWith<IllegalArgumentException> { Volume(-1, Volume.Units.QUART) }
+        assertFailsWith<IllegalArgumentException> { Volume(1, Volume.Units.QUART) * -1.5 }
+        assertFailsWith<IllegalArgumentException> { Volume(1, Volume.Units.QUART) - (gallon * 1.5) }
+        assertFailsWith<IllegalArgumentException> { Volume(1, Volume.Units.QUART) / -1 }
+    }
+
+    @Test fun conversion() {
+        assertEquals(tablespoon, teaspoon * 3)
+        assertEquals(ounce, tablespoon * 2)
+        assertEquals(cup, ounce * 8)
+        assertEquals(pint, cup * 2)
+        assertEquals(quart, pint * 2)
+        assertEquals(gallon, quart * 4)
+    }
+
+    @Test fun add() {
+        assertEquals(tablespoon + teaspoon, teaspoon * 4)
+    }
+
+    @Test fun substract() {
+        assertEquals(tablespoon - teaspoon, teaspoon * 2)
+        assertEquals(tablespoon - tablespoon, teaspoon * 0)
+    }
+
+    @Test fun divide() {
+        assertEquals(pint, quart / 2)
+        assertEquals(teaspoon * 0.5, teaspoon / 2)
     }
 }
